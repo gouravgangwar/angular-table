4d5b56eae313945566f8f3ae0e6317ff0bab4ef4
gouravgangwar <gouravgangwar6552@gmail.com>
2020-12-04 18:36:39 +0530

    1 filefgfg modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
+import { MatTableModule } from "@angular/material/table";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 
